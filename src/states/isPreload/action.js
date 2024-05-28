2dbd5cf9cbaee7a3b88088af583b96d495835024
widi-ps <widyapujisaputro@gmail.com>
2024-05-28 21:05:43 +0700

    first commit

---
+++ src/states/isPreload/action.js
@@ -0,0 +1,33 @@
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+
+/**
+ * @TODO: Define all the actions (creator) for the isPreLoad state
+ */
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+function setIsPreloadActionCreator(isPreLoad) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreLoad,
+    },
+  };
+}
+
+function asyncPreloadProcess() {
+  return async (dispatch) => {
+    try {
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      dispatch(setAuthUserActionCreator(null));
+    } finally {
+      dispatch(setIsPreloadActionCreator(false));
+    }
+  };
+}
+
+export { ActionType, setIsPreloadActionCreator, asyncPreloadProcess };
